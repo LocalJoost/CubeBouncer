c064e80c2c372d280353ccb05dc579a8fcec8862
Joost van Schaik <joost.van.schaik@wortell.nl>
2016-07-22 22:38:15 +0200

    Minor addemdum

--- Assets/Custom/Scripts/CubeManipulator.cs
+++ Assets/Custom/Scripts/CubeManipulator.cs
@@ -43,59 +43,62 @@ public class CubeManipulator : MonoBehaviour
             GazeManager.Instance.Position);
   }
 
-    void OnCollisionEnter(Collision coll)
+  void OnCollisionEnter(Collision coll)
+  {
+    // Ignore returning bodies
+    if (_rigidBody.isKinematic) return;
+
+    // Ignore hits by cursors
+    if (coll.gameObject.GetComponent<CursorManager>() != null) return;
+
+    // Play a click on hitting another cube, but only if the it has a higher Id
+    // to prevent the same sound being played twice
+    var othercube = coll.gameObject.GetComponent<CubeManipulator>();
+    if (othercube != null && othercube.Id < Id)
     {
-        // Ignore hits by cursors
-        if (coll.gameObject.GetComponent<CursorManager>() != null) return;
-
-        // Play a click on hitting another cube, but only if the it has a higher Id
-        // to prevent the same sound being played twice
-        var othercube = coll.gameObject.GetComponent<CubeManipulator>();
-        if (othercube != null && othercube.Id < Id)
-        {
-            _audioSource.PlayOneShot(BounceTogetherClip);
-        }
-
-        // No cursor, no cube - we hit a wall.
-        if (othercube == null)
-        {
-            if (coll.relativeVelocity.magnitude > 0.1)
-            {
-                _audioSource.PlayOneShot(BounceOtherClip);
-            }
-        }
+      _audioSource.PlayOneShot(BounceTogetherClip);
     }
 
-    public IEnumerator OnRevert()
+    // No cursor, no cube - we hit a wall.
+    if (othercube == null)
     {
-         var recallPosition = transform.position;
-         var recallRotation = transform.rotation;
+      if (coll.relativeVelocity.magnitude > 0.1)
+      {
+        _audioSource.PlayOneShot(BounceOtherClip);
+      }
+    }
+  }
 
-        _rigidBody.isKinematic = true;
+  public IEnumerator OnRevert()
+  {
+    var recallPosition = transform.position;
+    var recallRotation = transform.rotation;
 
-        while (transform.position != _orginalPosition &&
-               transform.rotation != _originalRotation)
-        {
-            yield return StartCoroutine(
-                MoveObject(transform, recallPosition, _orginalPosition,
-                           recallRotation, _originalRotation, 1.0f));
-        }
+    _rigidBody.isKinematic = true;
 
-        _rigidBody.isKinematic = false;
+    while (transform.position != _orginalPosition &&
+           transform.rotation != _originalRotation)
+    {
+      yield return StartCoroutine(
+          MoveObject(transform, recallPosition, _orginalPosition,
+                     recallRotation, _originalRotation, 1.0f));
     }
 
-    // See http://answers.unity3d.com/questions/711309/movement-script-2.html
-    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, 
-        Quaternion startRot, Quaternion endRot, float time)
+    _rigidBody.isKinematic = false;
+  }
+
+  // See http://answers.unity3d.com/questions/711309/movement-script-2.html
+  IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos,
+      Quaternion startRot, Quaternion endRot, float time)
+  {
+    var i = 0.0f;
+    var rate = 1.0f / time;
+    while (i < 1.0f)
     {
-        var i = 0.0f;
-        var rate = 1.0f / time;
-        while (i < 1.0f)
-        {
-            i += Time.deltaTime * rate;
-            thisTransform.position = Vector3.Lerp(startPos, endPos, Mathf.SmoothStep(0f, 1f, i));
-            thisTransform.rotation = Quaternion.Lerp(startRot, endRot, Mathf.SmoothStep(0f, 1f, i));
-            yield return null;
-        }
+      i += Time.deltaTime * rate;
+      thisTransform.position = Vector3.Lerp(startPos, endPos, Mathf.SmoothStep(0f, 1f, i));
+      thisTransform.rotation = Quaternion.Lerp(startRot, endRot, Mathf.SmoothStep(0f, 1f, i));
+      yield return null;
     }
+  }
 }
